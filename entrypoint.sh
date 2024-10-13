9f2e39a19c1841da6dd96a3b310d66b4a7463dac
psalmsin1759 <samson_ude@yahoo.com>
2024-10-13 23:52:31 +0100

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,21 @@ php artisan migrate --force
 # Start the Laravel queue worker
 # php artisan queue:work --tries=3
 
+php artisan db:seed --class=CategorySeeder
+
+php artisan db:seed --class=SliderSeeder
+
+php artisan db:seed --class=BannerSeeder
+
+php artisan db:seed --class=ProductSeeder
+
+php artisan db:seed --class=ProductCategorySeeder
+
+php artisan db:seed --class=RelatedProductSeeder
+
+php artisan db:seed --class=DeliveryMethodSeeder
+
 # Start the Laravel application
 exec php artisan serve --host=0.0.0.0 --port=8080
 
-exec php artisan db:seed --class=CategorySeeder
 
-exec php artisan db:seed --class=SliderSeeder
-
-exec php artisan db:seed --class=BannerSeeder
-
-exec php artisan db:seed --class=ProductSeeder
-
-exec php artisan db:seed --class=ProductCategorySeeder
-
-exec php artisan db:seed --class=RelatedProductSeeder
-
-exec php artisan db:seed --class=DeliveryMethodSeeder
